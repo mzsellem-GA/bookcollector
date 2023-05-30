51937e2643a6cb2d462b6d0d23d3950e8d165893
mzsellem-GA <125702378+mzsellem-GA@users.noreply.github.com>
2023-05-30 13:48:08 -0400

    book detail page

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 
-books = [
-    {'title': 'The Body Keeps The Score', 'genre': 'self-help', 'published': 2010},
-    {'title': 'Shadow and Bone', 'genre': 'fantasy', 'published': 2003},
-]
+from .models import Book
 
 # Create your views here.
 def home(request):
@@ -13,7 +10,13 @@ def about(request):
     # Include an .html file extension - unlike when rendering EJS templates
     return render(request, 'about.html')
 def books_index(request):
+    books = Book.objects.all()
     # Include an .html file extension - unlike when rendering EJS templates
     return render(request, 'books/index.html', {
     'books': books
+    })
+def books_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', {
+        'book': book
     })
