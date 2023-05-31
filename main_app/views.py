f2f037078e16b790c2ed66a24edabe84f27edf6c
mzsellem-GA <125702378+mzsellem-GA@users.noreply.github.com>
2023-05-30 22:09:54 -0400

    part 3 for lab

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Book
 
@@ -20,3 +21,15 @@ def books_detail(request, book_id):
     return render(request, 'books/detail.html', {
         'book': book
     })
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['title', 'genre', 'published']
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = '/books'
